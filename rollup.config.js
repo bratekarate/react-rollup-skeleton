e5daa1bd96404eb82734ab490e86d8340fbe245e
bratekarate <bratekannkarate@gmail.com>
2021-01-05 09:41:35 +0100

    improve dev server rollup flow

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import fs from "fs";
 import path from "path";
 import handler from 'serve-handler';
 import http from 'http';
+import {createHttpTerminator} from 'http-terminator';
 const fp = require('find-free-port');
 
 //const extensions = [".js", ".jsx", ".ts", ".tsx"];
@@ -14,17 +15,42 @@ const input = "src/index.tsx";
 
 const watch = process.env.ROLLUP_WATCH === 'true';
 
+let runningServer = null;
+
+async function serveStart() {
+  if (runningServer !== null) {
+    return;
+  }
+
+  const server = http.createServer((request, response) => {
+    return handler(request, response, {public: 'dist'});
+  })
+
+  const ports = await fp(8080);
+
+  server.listen({port: ports[0]}, () => {
+    console.log(`Running server at http://localhost:${ports[0]}`);
+  });
+
+  runningServer = {
+    terminator: createHttpTerminator({
+      server,
+    }),
+    port: ports[0]
+  };
+
+}
+
+async function serveStop() {
+  if (runningServer !== null) {
+    console.log(`Terminating server at http://localhost:${runningServer.port}`);
+    await runningServer.terminator.terminate();
+  }
+}
+
 const servePlugin = {
   buildStart: async () => {
-    const server = http.createServer((request, response) => {
-      return handler(request, response, {public: 'dist'});
-    })
-
-    const ports = await fp(8080);
-
-    server.listen({port: ports[0]}, () => {
-      console.log(`Running at http://localhost:${ports[0]}`);
-    });
+    await serveStart();
   }
 }
 
@@ -50,33 +76,39 @@ const external = [
   //...Object.keys(pkg.dependencies),
 ];
 
+const testSetup = async () => await serveStart();
+const testTeardown = async () => await serveStop();
+
 export default commandLineArgs => {
-  if (commandLineArgs.test === true) {
+  if (commandLineArgs.configTest === true) {
     plugins.push({
       buildStart: async () => {
-        console.log('prepare tests');
+        console.log('Preparing tests');
+        await testSetup();
       }
     });
     plugins.push({
       buildEnd: async () => {
-        console.log('run tests');
+        console.log('Running tests');
+        // TODO: run tests
+        console.log('Teardown tests');
+        await testTeardown();
       }
     });
   }
-  if (commandLineArgs.test === true ||
-    commandLineArgs.serve === true) {
-    plugins.push(servePlugin);    
+  if (commandLineArgs.configServe === true) {
+    plugins.push(servePlugin);
   }
   return [
-      {
-        input,
-        output: {
-          file: pkg.main,
-          format: "esm",
-          sourcemap: true,
-        },
-        external,
-        plugins,
+    {
+      input,
+      output: {
+        file: pkg.main,
+        format: "esm",
+        sourcemap: true,
       },
-    ]
-  }
+      external,
+      plugins,
+    },
+  ]
+}
